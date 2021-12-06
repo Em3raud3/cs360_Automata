ecd93598e5b190ab1088eafb9a8a25fa0393db93
emeraude <animekamisama@gmail.com>
2021-12-06 18:51:05 -0500

    added question 3

--- Machine.py
+++ Machine.py
@@ -30,28 +30,60 @@ class Machine:
         state = self.q0
         for s in s_input:
             state = self.δ[(state, s)]
-            
+
         if state in self.qf:
             return print("Accepted")
-        
+
         return print("Rejected")
 
 
 def main():
-    d = Machine()
-    d.Q = ["qo", "q1", "q2"]
-    d.Σ = ["a", "b"]
-    d.q0 = "q0"
-    d.qf = ["q2"]
+    #! Question 2
+    # d = Machine()
+    # d.Q = ["qo", "q1", "q2"]
+    # d.Σ = ["a", "b"]
+    # d.q0 = "q0"
+    # d.qf = ["q2"]
 
-    d.addArrow("q0", "a", "q0")
-    d.addArrow("q0", "b", "q1")
-    d.addArrow("q1", "a", "q1")
-    d.addArrow("q1", "b", "q2")
-    d.addArrow("q2", "a", "q2")
-    d.addArrow("q2", "b", "q2")
-    
-    d.DFA("bbaab")
+    # d.addArrow("q0", "a", "q0")
+    # d.addArrow("q0", "b", "q1")
+    # d.addArrow("q1", "a", "q1")
+    # d.addArrow("q1", "b", "q2")
+    # d.addArrow("q2", "a", "q2")
+    # d.addArrow("q2", "b", "q2")
+
+    # d.DFA("bbaab")
+
+    #! Question 3
+
+    d = Machine()
+
+    Q = input("Enter a set of states: ")
+    Q = Q.split(",")
+    d.Q = Q
+
+    Σ = input("Enter input alphabet: ")
+    Σ = Σ.split(",")
+    d.Σ = Σ
+
+    arrows = input("Enter transition rules (seperated by \"-\"): ")
+    arrows = arrows.split("-")  # This is a list of strings
+    for i in arrows:
+        arrow = i[1:-2] #removes parentheses
+        arrow = arrow.split(",")
+        d.addArrow(arrow[0], arrow[1], arrow[2])
+
+    q0 = input("Enter the start state: ")
+    d.q0 = q0
+
+    qf = input("Enter the final state: ")
+    d.qf = qf
+
+    DFA = input("Enter input string: ")
+    d.DFA(DFA)
+
+    d.description()
+
 
 if __name__ == "__main__":
     main()
